16fa508e3769c6a0660876a3799679abedf442e0
donjor <donjordev@gmail.com>
2018-06-15 13:06:43 +1200

    Add android privacy policy

---
+++ src/components/AndroidPrivacy.vue
@@ -0,0 +1,71 @@
+<template lang="html">
+  <div class="container">
+    <div class="site-section container">
+       <div>
+         <div>
+               <h1>Privacy Policy</h1>
+                <p>This page informs you of our policies regarding the collection,
+                use and disclosure of personal information we receive from users of our
+                app (havenwallet: Haven Wallet).
+                </p>
+                <p>By using the app, you agree to the collection and use of information in
+                accordance with this policy.
+                </p>
+           </div>
+            <div>
+                <h2>Data Collected</h2>
+                <p>Personal data is any kind of data that could identify an individual.
+                </p>
+                <p>Haven keys and public addresses are collected and processed by the app locally
+                for the purpose of processing transactions and transmitted into the Haven Network
+                in encrypted form.
+                </p>
+                <p>Other personal data is not collected by the app.</p>
+           </div>
+           <div>
+                <h2>App Permissions</h2>
+                <p>INTERNET : Connect to the Haven Network via a Haven Daemon Node</p>
+                <p>READ_EXTERNAL_STORAGE : Read wallet files stored on the device</p>
+                <p>WRITE_EXTERNAL_STORAGE : Write wallet files stored on the device</p>
+                <p>WAKE_LOCK : Keep device awake while syncing</p>
+                <p>CAMERA : Scan QR Codes for receiving Haven</p>
+           </div>
+           <div>
+                <h2>Changes to this Privacy Policy</h2>
+                <p>We may update this privacy policy from time to time. We will notify
+                you of any changes by posting the new privacy policy in the app and on the
+                website (havenwallet.com)
+                    You are advised to review this privacy policy periodically for any changes.</p>
+                <p>This Privacy Policy was last updated: 15th June, 2018.
+                </p>
+           </div>
+            <div>
+                <h2>Contact Us</h2>
+                <p>If you have any questions about our privacy policy,
+                or how your data is being collected and processed,
+                    please e-mail havencurrency@gmail.com.</p>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+
+<style lang="css">
+  .panel-body {
+    font-family: "Lucida Console", "Lucida Sans Typewriter", monaco, "Bitstream Vera Sans Mono", monospace;
+    font-size: 12px;
+    text-align: left;
+    color: #2bf22b;
+    background-color: #000;
+  }
+  .console {
+    overflow-wrap: break-word;
+    margin: auto;
+    max-width: 750px
+  }
+
+  .is-hidden {
+    visibility: hidden;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../components/Home'
+import AndroidPrivacy from '../components/AndroidPrivacy'
 
 Vue.use(Router)
 
@@ -15,6 +16,11 @@ export default new Router({
       path: '/post/:num',
       name: 'Post',
       component: Home
+    },
+    {
+      path: '/android-privacy-policy',
+      name: 'AndroidPrivacy',
+      component: AndroidPrivacy
     }
   ]
 })
